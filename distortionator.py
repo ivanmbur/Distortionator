b7266454d467978d3571b8f887b480a814f8d68a
Ivan Burbano <ivanmbur@gmail.com>
2017-08-14 21:00:24 -0700

    the variable names are improved, output of an ASCII file is included and an example too

--- distortionator.py
+++ distortionator.py
@@ -2,9 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from decimal import Decimal
 
-data_path = "/projector/cshapiro/analysis/2017-07-21/cleanpos/positions_clean_cds-f44spots_pupilbaf-_10_39_mega.npy"
-title = "Distortion parameters positions_clean_cds-f44spots_pupilbaf-_10_39_mega"
-name = "positions_clean_cds-f44spots_pupilbaf-_10_39_mega_parameters.png"
+data_path = ""
+title = ""
+plot_name = ""
+text_name = ""
 
 #Import chaz cube
 chaz_cube = np.load(data_path)
@@ -13,27 +14,27 @@ chaz_cube = np.load(data_path)
 sample_ref = np.average(chaz_cube, axis = 1)
 
 #Build the function which accepts 2 set of N vectors and returns the best affine transformation between them
-def affine_fit(set_1_raw, set_2_raw):
+def affine_fit(reference_raw, target_raw):
 	#We first make sure the sets are centered
-	center = np.average(set_1_raw, axis = 0)
-	set_1 = set_1_raw - center
-	set_2 = set_2_raw - center
+	center = np.average(reference_raw, axis = 0)
+	reference = reference_raw - center
+	target = target_raw - center
 
-	#We add a 1 to the end of every vector in set_1
-	set_1_extended = []
-	for vector in set_1:
-		set_1_extended.append(np.append(vector, [1]))
-	set_1_extended = np.array(set_1_extended)
+	#We add a 1 to the end of every vector in reference
+	reference_extended = []
+	for vector in reference:
+		reference_extended.append(np.append(vector, [1]))
+	reference_extended = np.array(reference_extended)
 
 	#We calculate the Q matrix
 	matrix_list = []
-	for vector in set_1_extended:
+	for vector in reference_extended:
 		vector_2D_transposed = vector[np.newaxis]
 		matrix_list.append(np.dot(vector_2D_transposed.T, vector_2D_transposed))
 	Q = np.sum(np.array(matrix_list), axis = 0)
 
 	#We calculate the c matrix
-	C = np.dot(set_2.T, set_1_extended)
+	C = np.dot(target.T, reference_extended)
 
 	#Solve the linear system
 	solution = [] 
@@ -45,13 +46,13 @@ def affine_fit(set_1_raw, set_2_raw):
 	return solution
 
 #Build a function that returns convergence, shear, rotation and displacement 
-def weak_variables(A):
+def distortion_parameters(A):
     return np.append(np.linalg.solve(np.array([[-1, -1, 0, 0], [0, 0, -1, -1], [0, 0, -1, 1], [-1, 1, 0, 0]]), np.array([A[0,0]-1, A[0,1], A[1,0], A[1,1]-1])), [A[0,2], A[1,2]])
 
 #Create time series of each weak variable
 weak_time_series = []
 for n in range(0,len(chaz_cube[0])):
-	weak_time_series.append(weak_variables(affine_fit(sample_ref, chaz_cube[:,n])))
+	weak_time_series.append(distortion_parameters(affine_fit(sample_ref, chaz_cube[:,n])))
 weak_time_series = np.array(weak_time_series)
 
 fig, ax = plt.subplots(len(weak_time_series[0]), figsize = (10, 15), sharex = True)
@@ -67,7 +68,9 @@ ax[3].set_ylabel("rotation ($\omega$)")
 ax[4].set_ylabel("translation ($b_1$)")
 ax[5].set_ylabel("translation ($b_2$)")
 plt.tight_layout()
-fig.savefig(name)
+fig.savefig(plot_name)
 plt.close(fig)
+
+np.savetxt(text_name, weak_time_series)
 
 
